57141dd7163d6750085230ae46d3d8db723aea71
Sabados Dan <danut.sabados@gmail.com>
2023-07-13 21:34:15 +0300

    Initial commit

---
+++ src/test/java/com/cwallet/cryptowallet/CryptoWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwallet.cryptowallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
